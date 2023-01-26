10ae5eaf3c29498c1b5ec98a50c5336959d09a19
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-01-26 12:02:06 +0200

    .

--- src/components/LoginNav.jsx
+++ src/components/LoginNav.jsx
@@ -1,12 +1,23 @@
 import "../css/LoginNav.css";
-import Logo from "../css/Logo.png";
+import Logo from "../css//media/Logo.png";
+import Discover from "../css//media/Discover.svg";
+import People from "../css//media/People.svg";
+import Jobs from "../css//media/Jobs.svg";
+import Learning from "../css//media/Learning.svg";
+
+import { Navbar } from "react-bootstrap";
 
 import React from "react";
 
 export default function LoginNav() {
   return (
     <div className="loginNav">
-      <img src={Logo} alt="logo" />
+      <Navbar className="w-75 m-auto p-0">
+        <a href="https://www.linkedin.com/feed/?trk=homepage-basic_signin-form_submit">
+          <img className="logopic" src={Logo} alt="logo" />
+          <hr></hr>
+        </a>
+      </Navbar>
     </div>
   );
 }
